fd567099eea81af00f19d5bdca606600490ab552
WangShuo <wangshuo866@gmail.com>
2018-01-04 11:21:24 +0800

    add title

--- pages/article.js
+++ pages/article.js
@@ -1,13 +1,16 @@
 import fetch from 'isomorphic-unfetch'
 
+import Head from '../components/Head'
 import MainHeader from '../components/MainHeader'
 import ArticleDetail from '../components/ArticleDetail'
+
 
 const API_URL = process.env.API_URL
 
 const Article = ({article}) => {
     return (
         <div>
+            <Head title={"iTeck - " + article.title}/>
             <MainHeader/>
             <ArticleDetail articleImg={article.image}
                            avatar={'/static/avatar.jpg'}
